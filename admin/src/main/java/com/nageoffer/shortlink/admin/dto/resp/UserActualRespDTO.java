9abb49052e8d667479e5dc803227a4d8e8f08424
buptLiYangCode <1642612105@qq.com>
2024-03-11 21:30:26 +0800

    feature：开发短链接后管系统用户信息脱敏

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/UserActualRespDTO.java
@@ -0,0 +1,26 @@
+package com.nageoffer.shortlink.admin.dto.resp;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.nageoffer.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
+import lombok.Data;
+
+/*
+用户返回参数响应
+ */
+@Data
+public class UserActualRespDTO {
+    // 用户ID
+    private Long id;
+
+    // 用户名
+    private String username;
+
+    // 真实姓名
+    private String realName;
+
+    // 手机号码
+    private String phone;
+
+    // 邮箱地址
+    private String mail;
+}
