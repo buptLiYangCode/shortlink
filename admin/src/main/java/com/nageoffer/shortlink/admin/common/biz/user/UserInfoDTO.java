fae1a665bd4ea522bf41a72b41d044b78543f275
buptLiYangCode <1642612105@qq.com>
2024-03-14 23:02:47 +0800

    feature：用户上下文利用ThreadLocal封装

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserInfoDTO.java
@@ -0,0 +1,21 @@
+package com.nageoffer.shortlink.admin.common.biz.user;
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserInfoDTO {
+    @JSONField(name = "id")
+    private String userId;
+
+    private String username;
+
+    private String realName;
+
+}
