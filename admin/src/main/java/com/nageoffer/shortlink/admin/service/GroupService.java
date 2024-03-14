ef83c8a8c61ab4d86afd163e916e1f4af2680bb4
buptLiYangCode <1642612105@qq.com>
2024-03-14 22:23:08 +0800

    feature：短链接分组集合

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
@@ -2,9 +2,14 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
+import com.nageoffer.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
+
+import java.util.List;
 
 public interface GroupService extends IService<GroupDO> {
 
     //新增短链接分组名
     void saveGroup(String groupName);
+
+    List<ShortLinkGroupRespDTO> listGroup();
 }
--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,14 +1,18 @@
 package com.nageoffer.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
 import com.nageoffer.shortlink.admin.dao.mapper.GroupMapper;
+import com.nageoffer.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.nageoffer.shortlink.admin.service.GroupService;
 import com.nageoffer.shortlink.admin.toolkit.RandomStringUtil;
 import groovy.util.logging.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 发呢组接口实现层
@@ -31,6 +35,20 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
                 .name(groupName)
                 .build();
         baseMapper.insert(groupDO);
+    }
+
+    /**
+     * 查询短链接分局集合
+     */
+    @Override
+    public List<ShortLinkGroupRespDTO> listGroup() {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getDelFlag, 0)
+                // TODO 从参数获取用户名
+                .eq(GroupDO::getUsername, "mading")
+                .orderByDesc(GroupDO::getSortOrder, GroupDO::getCreateTime);
+        List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+        return BeanUtil.copyToList(groupDOList, ShortLinkGroupRespDTO.class);
     }
 
     private boolean hasGid(String gid) {
