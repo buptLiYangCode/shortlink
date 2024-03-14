5072eb39a9b230ccdbf7ae3bca2c662e7a7fad57
buptLiYangCode <1642612105@qq.com>
2024-03-14 12:11:23 +0800

    hasGid：判断gid是否存在

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -0,0 +1,43 @@
+package com.nageoffer.shortlink.admin.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
+import com.nageoffer.shortlink.admin.dao.mapper.GroupMapper;
+import com.nageoffer.shortlink.admin.service.GroupService;
+import com.nageoffer.shortlink.admin.toolkit.RandomStringUtil;
+import groovy.util.logging.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 发呢组接口实现层
+ */
+@Slf4j
+@Service
+public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    @Override
+    public void saveGroup(String groupName) {
+        String gid;
+        do {
+            gid = RandomStringUtil.generateRandomString();
+        } while (!hasGid(gid));
+
+
+        GroupDO groupDO = GroupDO.builder()
+                .gid(gid)
+                .name(groupName)
+                .build();
+        baseMapper.insert(groupDO);
+    }
+
+    private boolean hasGid(String gid) {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getGid, gid)
+                // TODO 设置用户名
+                .eq(GroupDO::getUsername, null);
+        GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
+        return hasGroupFlag == null;
+    }
+}
